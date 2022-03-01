a2a88d99ccec72aba5949f4840e312dc807e9364
Ryan Bradshaw <ryan@rbradshaw.email>
2022-03-01 12:12:07 +0000

    updated workflow

--- infrastructure/global/vpc/variables.tf
+++ infrastructure/global/vpc/variables.tf
@@ -8,7 +8,7 @@ variable "public_subnet_1_cidr" {
   default     = "10.0.1.0/24"
 }
 variable "public_subnet_2_cidr" {
-  description = "CIDR Block for Public Subnet 2"
+  description = "CIDR Block for Public Subnet 2 NEW"
   default     = "10.0.2.0/24"
 }
 variable "private_subnet_1_cidr" {
