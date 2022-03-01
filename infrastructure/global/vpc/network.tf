da4f5eec0315635f93391f261c78051c995c7479
Ryan Bradshaw <ryan@rbradshaw.email>
2022-03-01 15:00:28 +0000

    added tag to vpc

--- infrastructure/global/vpc/network.tf
+++ infrastructure/global/vpc/network.tf
@@ -3,6 +3,10 @@ resource "aws_vpc" "talk-booking-vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  tags = {
+    Name = "TalkBooking"
+  }
 }
 
 # Public subnets
