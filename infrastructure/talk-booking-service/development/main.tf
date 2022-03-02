c910be0caed05991d158dbd5f317662229a22518
Ryan Bradshaw <ryan@rbradshaw.email>
2022-03-02 19:53:26 +0000

    updated terraform dev backend

--- infrastructure/talk-booking-service/development/main.tf
+++ infrastructure/talk-booking-service/development/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "talk-booking-terraform"
-    key = "terraform/tfstate"
+    key = "terraform/development/tfstate"
     region = "eu-west-2"
   }
 }
@@ -14,7 +14,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
     bucket = "talk-booking-terraform"
-    key = "terraform/tfstate"
+    key = "terraform/vpc/tfstate"
     region = "eu-west-2"
   }
 }
