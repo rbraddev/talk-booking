0ffd2e26cefb9e873348fca9bb471989818aea38
Ryan Bradshaw <ryan@rbradshaw.dev>
2022-03-16 18:20:02 +0000

    added request-talk endpoint

--- services/talk_booking/models/__init__.py
+++ services/talk_booking/models/__init__.py
@@ -1,4 +1,5 @@
 from .address import Address
 from .talk_request import TalkRequest
+from .api_requests import SubmitTalkRequest
 
-__all__ = ["Address", "TalkRequest"]
+__all__ = ["Address", "TalkRequest", "SubmitTalkRequest"]
--- services/talk_booking/web_app/main.py
+++ services/talk_booking/web_app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 
+from models import SubmitTalkRequest
+
 app = FastAPI()
 
 
 @app.get("/health-check/")
 def health_check():
     return {"message": "OK"}
+
+
+@app.post("/request-talk/", status_code=201)
+def request_talk(submit_talk_request: SubmitTalkRequest):
+    return {
+        "id": "unique_id",
+        "event_time": submit_talk_request.event_time,
+        "address": submit_talk_request.address,
+        "topic": submit_talk_request.topic,
+        "status": "PENDING",
+        "duration_in_minutes": submit_talk_request.duration_in_minutes,
+        "requester": submit_talk_request.requester,
+    }
